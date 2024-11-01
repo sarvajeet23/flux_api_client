bb8f233fa013b8f882b89469cb2a4dbe93b07e2c
Sarvajeet-infogem <skshibooofficial@gmail.com>
2024-11-01 15:23:04 +0530

    path

--- lib/flux_api_client.dart
+++ lib/flux_api_client.dart
@@ -1,6 +1,16 @@
 library flux_api_client;
 
-/// A Calculator.
+/// A library for handling various HTTP requests in Flutter.
+///
+/// This library provides classes for making GET, POST, PUT, and DELETE
+/// requests easily and efficiently.
+
+export 'delete_request.dart';
+export 'get_request.dart';
+export 'post_request.dart';
+export 'put_request.dart';
+
+/// A simple Calculator for demonstration purposes.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
